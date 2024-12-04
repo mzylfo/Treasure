b764ed364624cab773a86e3b66910846ff394119
mzylfo <m.zylfo@campus.unimib.it>
2024-12-04 19:35:20 +0100

    aggiunta libreria controllo email

--- app/src/main/java/com/example/treasure/LoginActivity.java
+++ app/src/main/java/com/example/treasure/LoginActivity.java
@@ -6,15 +6,15 @@ import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.textfield.TextInputEditText;
+import org.apache.commons.validator.routines.EmailValidator;
+
 
 public class LoginActivity extends AppCompatActivity {
 
     public static final String TAG = LoginActivity.class.getName();
+
     private TextInputEditText editTextEmail, editTextPassword;
 
     @Override
@@ -22,15 +22,27 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        /*
+
         editTextEmail = findViewById(R.id.textInputEmail);
         editTextPassword = findViewById(R.id.textInputPassword);
 
         Button loginButton = findViewById(R.id.loginButton);
 
         loginButton.setOnClickListener(view -> {
-            Log.d(TAG, "Email "+editTextEmail.getText());
-            Log.d(TAG, "Password "+editTextPassword.getText());
-        }); */
+            if (isEmailOk(editTextEmail.getText().toString()) && isPasswordOk(editTextPassword.getText().toString())) {
+                Log.d(TAG, "Launch new activity");
+            } else {
+                Log.d(TAG, "Error");
+            }
+        });
+
+    }
+
+    private boolean isEmailOk(String email) {
+        return EmailValidator.getInstance().isValid(email); //utilizzo la libreria di controllo
+    }
+
+    private boolean isPasswordOk(String password) {
+        return password.length() > 7;
     }
 }
