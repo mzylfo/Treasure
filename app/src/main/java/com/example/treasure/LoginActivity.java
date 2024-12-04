173408193bc142c4ece7d4e833f8cc450af01297
mzylfo <m.zylfo@campus.unimib.it>
2024-12-04 20:39:32 +0100

    aggiunta controllo email/password e accesso a nuova activity

--- app/src/main/java/com/example/treasure/LoginActivity.java
+++ app/src/main/java/com/example/treasure/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.example.treasure;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
@@ -7,6 +9,7 @@ import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 import org.apache.commons.validator.routines.EmailValidator;
 
@@ -29,10 +32,35 @@ public class LoginActivity extends AppCompatActivity {
         Button loginButton = findViewById(R.id.loginButton);
 
         loginButton.setOnClickListener(view -> {
-            if (isEmailOk(editTextEmail.getText().toString()) && isPasswordOk(editTextPassword.getText().toString())) {
-                Log.d(TAG, "Launch new activity");
-            } else {
-                Log.d(TAG, "Error");
+            if (isEmailOk(editTextEmail.getText().toString())){
+                if(isPasswordOk(editTextPassword.getText().toString())){
+                    // INTENT ESPLICITA
+                    Intent intent = new Intent(this, MainActivity.class);
+                    startActivity(intent);
+
+                    /*INTENT IMPLICITA
+                    Intent intent = new Intent();
+                    intent.setAction(intent.ACTION_SEND); //action send è l'azione di invio / inoltro verso un'altra applicazione
+                    intent.putExtra(intent.EXTRA_TEXT, "hello"); //definiamo il messaggio
+                    intent.setType("text/plain");
+
+                    // Try to invoke the intent.
+                    try {
+                        startActivity(intent);
+                    } catch (ActivityNotFoundException e) {
+                        // Define what your app should do if no activity can handle the intent.
+                    }
+                    */
+                }
+                else{
+                    Snackbar.make(findViewById(android.R.id.content), "Check your password", Snackbar.LENGTH_SHORT)
+                            .show();
+                }
+            }
+            else{
+                editTextEmail.setError("Check your email");
+                Snackbar.make(findViewById(android.R.id.content), "Insert correct email", Snackbar.LENGTH_SHORT)
+                        .show();
             }
         });
 
--- app/src/main/java/com/example/treasure/MainActivity.java
+++ app/src/main/java/com/example/treasure/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.treasure;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +10,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    public final static String TAG = MainActivity.class.getName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        //Log.d(TAG, savedInstanceState.getString("EMAIL_KEY"));
     }
 }
