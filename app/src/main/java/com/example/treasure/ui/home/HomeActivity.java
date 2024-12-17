01ad47e22e0e6cda3366f57e3e9e19740a2caf6c
mzylfo <m.zylfo@campus.unimib.it>
2024-12-17 21:12:59 +0100

    sistemazione generale

--- app/src/main/java/com/example/treasure/ui/home/HomeActivity.java
+++ app/src/main/java/com/example/treasure/ui/home/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.example.treasure.ui.home;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
@@ -17,6 +19,8 @@ import com.example.treasure.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
+import java.util.Objects;
+
 public class HomeActivity extends AppCompatActivity {
 
     @Override
@@ -27,8 +31,12 @@ public class HomeActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        // Disabilita il titolo predefinito della Toolbar
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
 
-         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().
+        TextView toolbarTitle = findViewById(R.id.toolbar_title);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().
                 findFragmentById(R.id.fragmentContainerView);
 
         NavController navController = navHostFragment.getNavController();
@@ -42,6 +50,12 @@ public class HomeActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(bottomNav, navController);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
+        // Osserva i cambiamenti di destinazione e aggiorna il titolo della toolbar
+        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+            if (destination.getLabel() != null) {
+                toolbarTitle.setText(destination.getLabel());
+            }
+        });
     }
 
 
