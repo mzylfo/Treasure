3419790725340676433b5d5cb407bcf5d44dc1a8
mzylfo <m.zylfo@campus.unimib.it>
2024-12-29 21:00:18 +0100

    room database (90%)

--- app/src/main/java/com/example/treasure/ui/home/fragment/HomeDailyFragment.java
+++ app/src/main/java/com/example/treasure/ui/home/fragment/HomeDailyFragment.java
@@ -1,7 +1,5 @@
 package com.example.treasure.ui.home.fragment;
 
-import static org.apache.commons.validator.GenericTypeValidator.formatDate;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,14 +9,12 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.example.treasure.R;
-import com.example.treasure.model.TimeZoneResponse;
 import com.example.treasure.util.Constants;
 import com.example.treasure.util.DateParser;
+import com.example.treasure.model.TimeZoneResponse;
 import com.example.treasure.util.JSONParserUtils;
 
 import java.io.IOException;
@@ -40,17 +36,22 @@ import java.util.Calendar;
 public class HomeDailyFragment extends Fragment {
     public static final String TAG = HomeDailyFragment.class.getName();
     private View nextUpView;
-
     private TextView dateTextView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home_daily, container, false);
-        dateTextView = view.findViewById(R.id.dateTextView);
-        nextUpView = view.findViewById(R.id.nextup);
+
         JSONParserUtils jsonParserUtils = new JSONParserUtils(getContext());
 
+        // Trova la view "nextup"
+        nextUpView = view.findViewById(R.id.nextup);
+
+        // Trova il TextView che contiene la data
+        dateTextView = view.findViewById(R.id.dateTextView);
+
+        //settiamo il text della data in modo formato
         try {
             TimeZoneResponse response = jsonParserUtils.parseJSONFileWithGSon(Constants.SAMPLE_JSON_FILENAME);
             String formattedDate = response.getFormatted();
@@ -60,24 +61,19 @@ public class HomeDailyFragment extends Fragment {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        // Aggiungi un listener di click alla view "nextup"
+        nextUpView.setOnClickListener(v -> {
+            // Ottieni la data dal TextView
+            String date = dateTextView.getText().toString();
 
-        nextUpView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showDailyPageFragment();
-            }
+            // Mostra il DailyPageFragment con la data ottenuta
+            DailyPageFragment dialogFragment = DailyPageFragment.newInstance(date);
+            dialogFragment.show(getParentFragmentManager(), "DailyPageFragment");
         });
 
-        //getCurrentTime();
         return view;
     }
-
-    private void showDailyPageFragment() {
-        DialogFragment dailyPageFragment = new DailyPageFragment();
-        dailyPageFragment.show(getFragmentManager(), "DailyPageFragment");
-    }
-/*
-    private void getCurrentTime() {
+    /*private void getCurrentTime() {
         OkHttpClient client = new OkHttpClient.Builder()
                 .connectTimeout(60, TimeUnit.SECONDS)
                 .readTimeout(60, TimeUnit.SECONDS)
@@ -90,18 +86,17 @@ public class HomeDailyFragment extends Fragment {
                 .build();
 
         ApiService apiService = retrofit.create(ApiService.class);
-        Call<TimeZoneResponse> call = apiService.getTimeZone("EFI0PITZ4VQC", "json", "zone", Constants.TIME_ZONE);
+        Call<TimeZoneResponse> call = apiService.getTimeZone("EFI0PITZ4VQC", "json", "zone", "Europe/Rome");
         call.enqueue(new Callback<TimeZoneResponse>() {
             @Override
             public void onResponse(@NonNull Call<TimeZoneResponse> call, @NonNull Response<TimeZoneResponse> response) {
                 if (response.isSuccessful() && response.body() != null) {
                     String formattedDate = response.body().getFormatted();
-                    String formattedDateWithNewFormat = formatDate(formattedDate);
+                    String formattedDateWithNewFormat = DateParser.formatDate(formattedDate);
                     dateTextView.setText(formattedDateWithNewFormat);
                 } else {
                     dateTextView.setText("Errore nella risposta");
-
-
+                    Log.e("HomeDailyFragment", "Response error: " + response.message());
                 }
             }
 
@@ -122,6 +117,5 @@ public class HomeDailyFragment extends Fragment {
                 @Query("zone") String zone
         );
     }
-*/
-
+    */
 }
